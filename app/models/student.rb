2554777307ca220a847dda34e72db1b45899a3a8
Apprentice <apprentice@dbc-chicago-026.local>
2014-02-12 16:41:37 -0600

    active record table building and validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,27 @@
 require_relative '../../db/config'
+require 'date'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    now = Date.today
+    now.year - self.birthday.year - ((now.month > self.birthday.month || (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
+  end
+
+  validates :email, uniqueness: true, format: { with: /.+@\w+\..{2,}/i, message: "must be a valid email address"}
+  validates :age, numericality: {greater_than: 5}
+  validate :phone_number_must_be_valid
+
+
+  def phone_number_must_be_valid
+    if self.phone.scan(/[0-9]/).size <10
+      errors.add(:phone, "number must have 10 digits")
+    end
+  end
 end
+
